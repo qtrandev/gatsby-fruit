4b1e103a07862739c41187aa0bf8f69780b5003c
Quyen Tran <qtrandev@gmail.com>
2019-09-13 00:01:43 +0700

    Add mango picture on main page

--- src/components/image.js
+++ src/components/image.js
@@ -16,9 +16,9 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+      placeholderImage: file(relativePath: { eq: "mangospread.jpg" }) {
         childImageSharp {
-          fluid(maxWidth: 300) {
+          fluid(maxWidth: 1200) {
             ...GatsbyImageSharpFluid
           }
         }
